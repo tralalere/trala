c685b57bb1fd9ea269894677fea134d2f2184d63
Florent Fortat <florent.fortat@tralalere.com>
2018-04-20 17:29:09 +0200

    Fill manifest format interface and implement add / remove / get

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,5 +1,16 @@
 interface ManifestFormat {
+    project: string;
+    remoteUrl: string;
+    namespace: string;
+    version?: string;
+    modules: {[name: string]: ModuleFormat};
+}
 
+interface ModuleFormat {
+    version: string;
+    remoteUrl?: string;
+    namespace?: string;
+    repository?: string;
 }
 
 export class Manifest {
@@ -15,7 +26,9 @@ export class Manifest {
     private manifestData: ManifestFormat;
 
     constructor() {
+        // TODO get/save manifest path file
 
+        this.loadManifest();
     }
 
     public addModules(modules: string[][]) {
@@ -46,15 +59,27 @@ export class Manifest {
     public getModules(): string[][] {
         const modules: string[][] = [];
 
+        for (const module in this.manifestData.modules) {
+            modules.push([module, this.manifestData.modules[module].version]);
+        }
+
         return modules;
     }
 
     private addModuleInternal(name: string, version: string) {
-
+        if (!this.manifestData.modules[name]) {
+            this.manifestData.modules[name] = { version };
+        }
     }
 
     private removeModuleInternal(name: string) {
+        if (this.manifestData.modules[name]) {
+            delete this.manifestData.modules[name];
+        }
+    }
 
+    private loadManifest() {
+        // TODO load manifest
     }
 
     private saveManifest() {
