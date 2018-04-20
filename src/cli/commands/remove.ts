af3f55bcf1081217d99b61f1de3c58f252245c06
Florent Fortat <florent.fortat@tralalere.com>
2018-04-20 15:59:43 +0200

    Implement remove command structure

--- src/cli/commands/remove.ts
+++ src/cli/commands/remove.ts
@@ -1,17 +1,24 @@
+import {Manifest} from "../../manifest";
+
 export class Remove{
     static execute(args: string[]) {
         console.log('remove', args);
 
-        // TODO get module(s)
+        const manifest = Manifest.getInstance();
+        let modules: string[];
+
+        modules = args.slice().map((arg: string) => arg.split('@')[0]);
+
+        modules.forEach((module: string) => Remove.removeModule(module));
+
+        manifest.removeModules(modules);
+    }
+
+    static removeModule(name: string) {
+        console.log('remove module', name);
 
         // TODO delete module repository
 
-        // TODO remove module from trllr.json
-
         // TODO update files to remove module inclusion
-    }
-
-    static removeModule(name: string, version?: string) {
-
     }
 }
