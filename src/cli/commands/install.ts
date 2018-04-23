5464dd6cc1d5e66e7fbcffb1496fd5ea30da9981
Florent Fortat <florent.fortat@tralalere.com>
2018-04-23 18:57:24 +0200

    Implementation of module installation with git repo cloning, branch listing (WIP)

--- src/cli/commands/install.ts
+++ src/cli/commands/install.ts
@@ -1,10 +1,15 @@
 import { Manifest } from "../../manifest";
+import {execSync} from "child_process";
 
 export class Install {
+    static manifest: Manifest;
+    static projectName: string;
+
     static execute(args: string[]) {
         console.log('install', args);
 
-        const manifest = Manifest.getInstance();
+        this.manifest = Manifest.getInstance();
+        this.projectName = this.manifest.getProjectName().toLowerCase();
         let modules: string[][];
         let updateManifest: boolean = true;
 
@@ -12,23 +17,68 @@ export class Install {
 
         if (modules.length === 0) {
             updateManifest = false;
-            modules = manifest.getModules();
+            modules = this.manifest.getModules();
         }
 
-        modules.forEach((module: string[]) => Install.installModule(module[0], module[1]));
+        modules.forEach((module: string[]) => module[1] = this.installModule(module[0], module[1]));
 
         if (updateManifest) {
-            manifest.addModules(modules);
+            this.manifest.addModules(modules);
         }
     }
 
-    static installModule(name: string, version?: string) {
+    static installModule(name: string, version?: string): string {
         console.log('install module', name, version);
 
-        // TODO clone module repository and checkout branch for project
+
+        const rootDir: string = process.cwd();
+        const branchName: string = `project-${this.projectName}`;
+        let branches: string[];
+        let branchPresent: boolean = false;
+        let branchLocal: boolean = false;
+        let currentVersion: string;
+        let useVersion: string;
+
+        execSync(`git clone ${this.manifest.getRemoteUrl()}${this.manifest.getNamespace()}/module-${name}-front src/app/@modules/${name}`);
+        process.chdir(`src/app/@modules/${name}`);
+
+        execSync('git fetch origin');
+        branches = execSync('git branch -a', { encoding: 'utf8' })
+            .split('\n')
+            .filter((branch: string) => {
+                const branchIndex = branch.indexOf(branchName);
+                return branchIndex > -1 && branchIndex + branchName.length === branch.length;
+            });
+
+        if (branches.length) {
+            branchPresent = branches.some((branch: string) => branch.indexOf('remotes') > -1);
+            branchLocal = branches.some((branch: string) => branch.indexOf('remotes') === -1);
+        }
+
+        // TODO if project branch exists, get current version
+
+        // TODO list tags
+
+        if (version) {
+            // TODO check if version is present
+            useVersion = version;
+        } else if (currentVersion) {
+            useVersion = currentVersion;
+        } else {
+            // TODO get latest version from tags
+            useVersion = '0.1.0'
+        }
 
         // TODO update version (branches if needed) and push changes
+        process.chdir(rootDir);
 
+        this.includeModule(name);
+
+        return useVersion;
+    }
+
+    static includeModule(name: string) {
         // TODO update files to include module
+
     }
 }
