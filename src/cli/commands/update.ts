d17c75c6b9135f881cb6dbf62cbb0ce162ba0c53
Florent Fortat <florent.fortat@tralalere.com>
2018-09-12 18:25:37 +0200

    Exec git commands to update project and modules

--- src/cli/commands/update.ts
+++ src/cli/commands/update.ts
@@ -1,31 +1,56 @@
 import { Install } from "./install";
 import { Remove } from "./remove";
 import { Manifest } from "../../manifest";
+import {execSync} from "child_process";
 
 /**
  * Implement Update command
  */
 export class Update {
+    static manifest: Manifest;
+
     /**
      * Perform an update on a module by removing it and installing it again
      * @param {string[]} args (arguments)
      */
     public static execute(args: string[]) {
-        const manifest = Manifest.getInstance();
+        this.manifest = Manifest.getInstance();
         let modules: string[][];
 
         modules = args.slice().map((arg: string) => arg.split('@'));
 
-        // TODO if no modules update all modules (?) (maybe not)
+        if (modules.length) {
+            modules.forEach((module) => {
+                Remove.removeModule(module[0], true);
 
-        modules.forEach((module) => {
-            Remove.removeModule(module[0], true);
+                Install.installModule(module[0], module[1]);
+            });
 
-            Install.installModule(module[0], module[1]);
-        });
+            this.manifest.updateModules(modules);
+        } else {
+            console.log('Updating the project and all modules');
 
-        manifest.updateModules(modules);
+            this.updateRepository('.');
+            console.log('Project updated');
+
+            this.manifest.reloadManifest();
+
+            modules = this.manifest.getModules();
+            modules.forEach((module) => {
+                this.updateRepository('src/app/@modules/'+module[0]);
+                console.log('Module', module[0], 'updated');
+            });
+        }
 
         console.log('Done!');
+    }
+
+    /**
+     * Execute fetch and pull git commands on the given path
+     * @param {string} path (path to git repository)
+     */
+    private static updateRepository(path: string) {
+        execSync('git --git-dir='+path+'/.git fetch', {stdio: 'ignore'});
+        execSync('git --git-dir='+path+'/.git pull', {stdio: 'ignore'});
     }
 }
