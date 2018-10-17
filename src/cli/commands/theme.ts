65e7b308ad164bc7fae20fa204b4cfe06e19dc9d
Florent Fortat <florent.fortat@tralalere.com>
2018-10-17 12:33:33 +0200

    Complete implementation of theme switching

--- src/cli/commands/theme.ts
+++ src/cli/commands/theme.ts
@@ -16,11 +16,14 @@ export class Theme {
     this.manifest = Manifest.getInstance();
     this.projectName = this.manifest.getProjectName().toLowerCase();
 
-    console.log('switching to', args[0]);
-    const themeName = args[0];
+    const themeName = args[0].replace(/\//g, '');
+    const basePath = '';
+
+    console.log('Switching theme to', themeName);
 
     // Edit angular.json
-    const angularManifest = JSON.parse(readFileSync('angular.json', { encoding: 'utf-8' }));
+    const angularManifestPath = `${basePath}angular.json`;
+    const angularManifest = JSON.parse(readFileSync(angularManifestPath, { encoding: 'utf-8' }));
     const buildOptions = angularManifest.projects.fuse.architect.build.options;
 
     buildOptions.assets = buildOptions.assets.map((path: string) => {
@@ -41,37 +44,42 @@ export class Theme {
       return path;
     });
 
-    writeFileSync('angular.json', JSON.stringify(angularManifest, null, '    ')+'\n', { encoding: 'utf-8' });
+    writeFileSync(angularManifestPath, JSON.stringify(angularManifest, null, '    ')+'\n', { encoding: 'utf-8' });
 
-    // TODO edit src/styles.scss
-    const styles = readFileSync('src/styles.scss', { encoding: 'utf-8' });
-    const pattern = 'app/themes/';
-    const patternIndex = styles.indexOf(pattern);
+    // Edit src/styles.scss
+    const stylesPath = `${basePath}src/styles.scss`;
+    const styles = readFileSync(stylesPath, { encoding: 'utf-8' });
+    const newStyles = styles.replace(/(@import.*app\/themes\/)([^\/]*)(\/.*;)/g, `$1${themeName}$3`);
 
-    // TODO use string.replace to make it more straight forward, build a regexp to match @import "app/themes/(.*)/.*"; and replace group 1 with themeName
-    // TODO use same logic in every file
+    writeFileSync(stylesPath, newStyles, { encoding: 'utf-8' });
 
-    if (patternIndex > -1) {
-      const slashIndex = styles.indexOf('/', patternIndex + pattern.length);
-      let newStyles = styles.slice(0, patternIndex + pattern.length) + themeName;
+    // Edit src/app/app.module.ts
+    const appModulePath = `${basePath}src/app/app.module.ts`;
+    const appModule = readFileSync(appModulePath, { encoding: 'utf-8' });
+    const newAppModule = appModule.replace(/(import.*\/themes\/)([^\/]*)(\/.*;)/g, `$1${themeName}$3`);
 
-      if (slashIndex > -1) {
+    writeFileSync(appModulePath, newAppModule, { encoding: 'utf-8' });
 
-      } else {
+    // Edit src/app/service-instantiator.class.ts
+    const serviceInstantiatorPath = `${basePath}src/app/service-instantiator.class.ts`;
+    const serviceInstantiator = readFileSync(serviceInstantiatorPath, { encoding: 'utf-8' });
+    const newServiceInstantiator = serviceInstantiator.replace(/(import.*\/themes\/)([^\/]*)(\/.*;)/g, `$1${themeName}$3`);
 
-      }
-      console.log(newStyles);
+    writeFileSync(serviceInstantiatorPath, newServiceInstantiator, { encoding: 'utf-8' });
 
-      // writeFileSync('src/styles.scss', newStyles, { encoding: 'utf-8' });
-    }
+    // Edit src/app/settings.ts
+    const settingsPath = `${basePath}src/app/settings.ts`;
+    const settings = readFileSync(settingsPath, { encoding: 'utf-8' });
+    const newSettings = settings.replace(/(export.*brand[^'"]*['"])([^'"]*)([^;];)/g, `$1${themeName}$3`);
 
-    // TODO edit src/app/app.module.ts
+    writeFileSync(settingsPath, newSettings, { encoding: 'utf-8' });
 
-    // TODO edit src/app/service-instantiator.class.ts
+    // Edit src/app/themes/theme.scss
+    const themePath = `${basePath}src/app/themes/theme.scss`;
+    const theme = readFileSync(themePath, { encoding: 'utf-8' });
+    const newTheme = theme.replace(/(@import.*\.\/)([^\/]*)(\/.*;)/g, `$1${themeName}$3`);
 
-    // TODO edit src/app/settings.ts
-
-    // TODO edit src/app/themes/theme.scss
+    writeFileSync(themePath, newTheme, { encoding: 'utf-8' });
 
   }
 }
