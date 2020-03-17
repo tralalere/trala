17f4c3553f213308b4daf29b5ae65969de02cfb1
Florent Fortat <florent@fortat.fr>
2020-03-17 12:17:52 +0100

    Update default gitlab host url

--- src/manifest.ts
+++ src/manifest.ts
@@ -24,7 +24,7 @@ interface ModuleFormat {
 const defaultFilename: string = 'trllr';
 const defaultData: ManifestFormat = {
     project: '',
-    remoteUrl: 'git@gitlab.abc-informatik.fr:',
+    remoteUrl: 'git@git.tralalere.com:',
     namespace: 'lms_trala',
     modules: {}
 };
